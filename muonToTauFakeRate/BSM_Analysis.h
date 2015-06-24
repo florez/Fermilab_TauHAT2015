7d63becb9005649a256016d05928691663cf4d53
florez <andres.florez@cern.ch>
2015-06-24 17:42:02 -0500

    updating electrons objects

--- muonToTauFakeRate/BSM_Analysis.h
+++ muonToTauFakeRate/BSM_Analysis.h
@@ -80,6 +80,24 @@ public :
    vector<double>  *Muon_isoNeutralHadron;
    vector<double>  *Muon_isoPhoton;
    vector<double>  *Muon_isoPU;
+   vector<double>  *patElectron_pt;
+   vector<double>  *patElectron_eta;
+   vector<double>  *patElectron_phi;
+   vector<double>  *patElectron_energy;
+   vector<double>  *patElectron_charge;
+   vector<int>     *patElectron_isPassVeto;
+   vector<int>     *patElectron_isPassLoose;
+   vector<int>     *patElectron_isPassMedium;
+   vector<int>     *patElectron_isPassTight;
+   vector<int>     *patElectron_isPassHEEPId;
+   vector<double>  *patElectron_d0;
+   vector<double>  *patElectron_dz;
+   vector<int>     *patElectron_expectedMissingInnerHits;
+   vector<int>     *patElectron_passConversionVeto;
+   vector<double>  *patElectron_isoChargedHadrons;
+   vector<double>  *patElectron_isoNeutralHadrons;
+   vector<double>  *patElectron_isoPhotons;
+   vector<double>  *patElectron_isoPU;
    vector<double>  *Tau_eta;
    vector<double>  *Tau_phi;
    vector<double>  *Tau_pt;
@@ -175,6 +193,24 @@ public :
    TBranch        *b_Muon_isoNeutralHadron;   //!
    TBranch        *b_Muon_isoPhoton;   //!
    TBranch        *b_Muon_isoPU;   //!
+   TBranch        *b_patElectron_pt;   //!
+   TBranch        *b_patElectron_eta;   //!
+   TBranch        *b_patElectron_phi;   //!
+   TBranch        *b_patElectron_energy;   //!
+   TBranch        *b_patElectron_charge;   //!
+   TBranch        *b_patElectron_isPassVeto;   //!
+   TBranch        *b_patElectron_isPassLoose;   //!
+   TBranch        *b_patElectron_isPassMedium;   //!
+   TBranch        *b_patElectron_isPassTight;   //!
+   TBranch        *b_patElectron_isPassHEEPId;   //!
+   TBranch        *b_patElectron_d0;   //!
+   TBranch        *b_patElectron_dz;   //!
+   TBranch        *b_patElectron_expectedMissingInnerHits;   //!
+   TBranch        *b_patElectron_passConversionVeto;   //!
+   TBranch        *b_patElectron_isoChargedHadrons;   //!
+   TBranch        *b_patElectron_isoNeutralHadrons;   //!
+   TBranch        *b_patElectron_isoPhotons;   //!
+   TBranch        *b_patElectron_isoPU;   //!
    TBranch        *b_Tau_eta;   //!
    TBranch        *b_Tau_phi;   //!
    TBranch        *b_Tau_pt;   //!
