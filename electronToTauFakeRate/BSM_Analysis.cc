aa55cca88f494fc757469665887f58b75d030b77
florez <andres.florez@cern.ch>
2015-07-06 23:39:41 -0500

    updating electron class

--- electronToTauFakeRate/BSM_Analysis.cc
+++ electronToTauFakeRate/BSM_Analysis.cc
@@ -110,21 +110,19 @@ BSM_Analysis::BSM_Analysis(TFile* theFile, TDirectory *cdDir[], int nDir, char* 
 	  } 
           if (!found_tag_electron){continue;}
 	  // Now we loop over the taus in the event, if any 
-	  for (int t = 0; t < Tau_pt->size(); t++)
-	    {
+          for (int t = 0; t < Tau_pt->size(); t++)
+            {
               ProbeTau_TL_vec.SetPtEtaPhiE(Tau_pt->at(t), Tau_eta->at(t), Tau_phi->at(t), Tau_energy->at(t));
               double DeltaR_electron_tau = TagElectron_TL_vec.DeltaR(ProbeTau_TL_vec);
-	      double charge_product = (patElectron_charge->at(j))*(Tau_charge->at(t));
-	      
-	      if ((DeltaR_electron_tau < 0.5) || (charge_product >=0)) {continue;}
-              if (Tau_pt->at(t) < 20.0){continue;}
-              if (abs(Tau_eta->at(t)) > 2.3){continue;}
-	      if( Tau_decayModeFinding->at(t) == 1){
-		pass_tau_id[1] = 1;
-	      }
+              double charge_product = (patElectron_charge->at(j))*(Tau_charge->at(t));
+              if ((DeltaR_electron_tau > 0.5) && (charge_product < 0) && (Tau_pt->at(t) > 20) && (abs(Tau_eta->at(t)) > 2.3)){
+              if( Tau_decayModeFinding->at(t) == 1){
+                pass_tau_id[1] = 1;
+              }
               found_probe_tau = true;
               break;
-	    }
+              }
+            }
            if (found_probe_tau){break;}
 	}
       
